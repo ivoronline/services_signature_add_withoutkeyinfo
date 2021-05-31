25f0685100d5ab6cc0d5e85760d9d572312cecae
igoreta <ivoronline@gmail.com>
2021-05-31 18:19:22 +0200

    Commit

--- src/main/java/AddSignatureWithoutKeyInfo.java
+++ src/main/java/AddSignatureWithoutKeyInfo.java
@@ -20,18 +20,18 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.OutputStream;
 import java.security.KeyStore;
-import java.security.cert.X509Certificate;
+import java.security.PrivateKey;
 import java.util.Collections;
 
-public class AddSIgnatureWithoutKeyInfo {
+public class AddSignatureWithoutKeyInfo {
 
-  //KEY STOREa
+  //KEY STORE
   static String keyStoreName     = "src/main/resources/ClientKeyStore.jks";
   static String keyStorePassword = "mypassword";
   static String keyStoreType     = "JKS";
   static String keyAlias         = "clientkeys1";
 
-  //SIGN DOCUMENT
+  //XML FILES
   static String xmlInput         = "src/main/resources/Person.xml";
   static String xmlSigned        = "src/main/resources/PersonSignedWithoutKeyInfo.xml";
 
@@ -40,32 +40,43 @@ public class AddSIgnatureWithoutKeyInfo {
   //================================================================================
   public static void main(String[] args) throws Exception {
 
-    //LOG
-    System.out.println("MyRunnerWithoutKeyInfo");
-
-    //CREATE XML SIGNATURE FACTORY
-    XMLSignatureFactory factory = XMLSignatureFactory.getInstance("DOM");
-
-    //GET KEY STORE, PRIVATE & PUBLIC KEY (CERTIFICATE)
-    KeyStore                    keyStore    = KeyStore.getInstance(keyStoreType);
-                                keyStore.load(new FileInputStream(keyStoreName), keyStorePassword.toCharArray());
-    KeyStore.PasswordProtection keyPassword = new KeyStore.PasswordProtection(keyStorePassword.toCharArray());
-    KeyStore.PrivateKeyEntry    keyPair= (KeyStore.PrivateKeyEntry) keyStore.getEntry(keyAlias, keyPassword);
-    X509Certificate             certificate = (X509Certificate) keyPair.getCertificate();
-
-    //LOAD INPUT XML DOCUMENT
+    //GET DOCUMENT (from XML file)
     DocumentBuilderFactory documentFactory = DocumentBuilderFactory.newInstance();
                            documentFactory.setNamespaceAware(true);
-    Document               document        = documentFactory.newDocumentBuilder().parse(new FileInputStream(xmlInput));
+    Document document    = documentFactory.newDocumentBuilder().parse(new FileInputStream(xmlInput));
 
-    //CREATE SIGN CONTEXT
-    DOMSignContext domSignContext = new DOMSignContext(keyPair.getPrivateKey(), document.getDocumentElement());
+    //SIGN DOCUMENT
+    signDocument(document);
+
+    //CREATE OUTPUT XML FILE
+    OutputStream       outputStream       = new FileOutputStream(xmlSigned);
+    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+    Transformer        transformer        = transformerFactory.newTransformer();
+                       transformer.transform(new DOMSource(document), new StreamResult(outputStream));
+
+  }
+
+  //================================================================================
+  // SIGN DOCUMENT
+  //================================================================================
+  public static Document signDocument(Document document) throws Exception {
+
+    //GET PRIVATE KEY
+    char[]                      password    = keyStorePassword.toCharArray();  //The same for KeyStore & Private Key
+    KeyStore                    keyStore    = KeyStore.getInstance(keyStoreType);
+                                keyStore.load(new FileInputStream(keyStoreName), password);
+    KeyStore.PasswordProtection keyPassword = new KeyStore.PasswordProtection(   password);
+    KeyStore.PrivateKeyEntry    keyPair     = (KeyStore.PrivateKeyEntry) keyStore.getEntry(keyAlias, keyPassword);
+    PrivateKey                  privateKey  = keyPair.getPrivateKey();
 
     //CREATE REFERENCE
-    Reference reference = factory.newReference(
+    XMLSignatureFactory factory   = XMLSignatureFactory.getInstance("DOM");
+    Reference           reference = factory.newReference(
       "",
       factory.newDigestMethod(DigestMethod.SHA1, null),
-      Collections.singletonList(factory.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null)),null, null
+      Collections.singletonList(factory.newTransform(Transform.ENVELOPED, (TransformParameterSpec) null)),
+      null,
+      null
     );
 
     //SPECIFY SIGNATURE TYPE
@@ -75,14 +86,12 @@ public class AddSIgnatureWithoutKeyInfo {
     );
 
     //SIGN DOCUMENT
-    XMLSignature signature = factory.newXMLSignature(signedInfo, null);
-                 signature.sign(domSignContext);
+    DOMSignContext domSignContext = new DOMSignContext(privateKey, document.getDocumentElement());
+    XMLSignature   signature      = factory.newXMLSignature(signedInfo, null);
+                   signature.sign(domSignContext);
 
-    //CREATE OUTPUT XML DOCUMENT
-    OutputStream       outputStream       = new FileOutputStream(xmlSigned);
-    TransformerFactory transformerFactory = TransformerFactory.newInstance();
-    Transformer        transformer        = transformerFactory.newTransformer();
-                       transformer.transform(new DOMSource(document), new StreamResult(outputStream));
+    //RETURN DOCUMENT
+    return document;
 
   }
 
